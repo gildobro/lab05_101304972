aace2b6a7197b45220b670bd9add634f55b2b923
Gil Dobrovinsky <gdobrovinsky@gmail.com>
2023-03-08 19:24:45 -0500

    exercise-2

---
+++ exercise-2/customer.ts
@@ -0,0 +1,14 @@
+class Customer {
+    firstName: string;
+    lastName: string;
+
+    public greeter(){
+        console.log(`Hello ${this.firstName} ${this.lastName}`);
+    }
+}
+
+let customer = new Customer();
+customer.firstName = "Gil";
+customer.lastName = "Dobrovinsky";
+
+customer.greeter();
